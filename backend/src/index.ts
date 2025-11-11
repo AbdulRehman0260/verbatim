05d8d218d3a796e54ba18c93dbf733ad02c7398f
Abdul Rehman Shoukat <abdulzain19@gmail.com>
2025-11-11 13:15:00 +0000

    git initialized

---
+++ backend/src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+
+const app = express();
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
